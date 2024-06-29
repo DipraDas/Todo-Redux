a6c6d06b9e80ef086b1e212551d39c104ac51bab
Dipra <dipradas5940@gmail.com>
2024-06-29 18:48:41 +0600

    RTK Query Initial Setup

---
+++ src/redux/api/Api.ts
@@ -0,0 +1,11 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const pokemonApi = createApi({
+    reducerPath: 'pokemonApi',
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+    endpoints: () => ({
+
+    }),
+})
+
+// export const { useGetPokemonByNameQuery } = pokemonApi
